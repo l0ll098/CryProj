3a69c1ab9b2116575327f42ee017feb59476ac5a
Lorenzo Montanari <lorenzo0.montanari@gmail.com>
2018-03-18 20:59:18 +0100

    Now the extension code is wrapped in a class

--- src/extension.ts
+++ src/extension.ts
@@ -4,7 +4,7 @@ import * as vscode from "vscode";
 
 enum CRYENGINE_VERSIONS {
     "engine-dev",
-    "engine-5.4", 
+    "engine-5.4",
     "engine-5.3",
     "engine-5.2"
 }
@@ -12,45 +12,64 @@ enum CRYENGINE_VERSIONS {
 
 export function activate(context: vscode.ExtensionContext) {
     console.log("CryProj extension is active");
-
-    // create a new word counter
+    
     let cryEngineVersion = new CryEngineVersion();
+    let extController = new ExtensionController(context, cryEngineVersion);
 
-    // This will trigger the inner function when user changes file in the editor
-    vscode.window.onDidChangeActiveTextEditor((e: vscode.TextEditor) => {
-        if (isACryProjFile(e)) {
-            cryEngineVersion.updateVersion();
-        } else {
-            cryEngineVersion.hideStatusBar();
-        }
-    });
-
-    // This will be triggered when user changes something in a file
-    vscode.window.onDidChangeTextEditorSelection((e: vscode.TextEditorSelectionChangeEvent)=>{
-        if(isACryProjFile(e.textEditor)){
-            cryEngineVersion.updateVersion();
-        }
-    })
-
-    // Required as if user opens VS Code with a file already opened it won't trigger
-    // the onDidChangeActiveTextEditor, so checks are needed even there
-    if (isACryProjFile(vscode.window.activeTextEditor)) {
-        cryEngineVersion.updateVersion();
-    }
-    // Add to a list of disposables which are disposed when this extension is deactivated.
     context.subscriptions.push(cryEngineVersion);
+    context.subscriptions.push(extController);
 }
 
 export function deactivate() { }
 
-function isACryProjFile(e: vscode.TextEditor) {
-    return e.document.fileName.toLowerCase().endsWith(".cryproj");
-    // TODO: 
-    // When CryProj and JSON files will be two different thinghs, change the line above 
-    // with this one
-    //return e.document.languageId.toLowerCase() === "cryproj";
-}
 
+class ExtensionController {
+    private subscriptions: vscode.Disposable[] = [];
+    private disposable: vscode.Disposable;
+
+    constructor(context: vscode.ExtensionContext, cryEngineVersion: CryEngineVersion) {
+
+        this.subscriptions = [];
+
+        // This will trigger the inner function when user changes file in the editor
+        vscode.window.onDidChangeActiveTextEditor((e: vscode.TextEditor) => {
+            if (this.isACryProjFile(e)) {
+                cryEngineVersion.updateVersion();
+            } else {
+                cryEngineVersion.hideStatusBar();
+            }
+        }, null, this.subscriptions);
+
+        // This will be triggered when user changes something in a file
+        vscode.window.onDidChangeTextEditorSelection((e: vscode.TextEditorSelectionChangeEvent) => {
+            if (this.isACryProjFile(e.textEditor)) {
+                cryEngineVersion.updateVersion();
+            }
+        }, null, this.subscriptions);
+
+        // Required as if user opens VS Code with a file already opened it won't trigger
+        // the onDidChangeActiveTextEditor, so checks are needed even there
+        if (this.isACryProjFile(vscode.window.activeTextEditor)) {
+            cryEngineVersion.updateVersion();
+        }
+        // Add to a list of disposables which are disposed when this extension is deactivated.
+        context.subscriptions.push(cryEngineVersion);
+
+        this.disposable = vscode.Disposable.from(...this.subscriptions);
+    }
+
+    public dispose() {
+        this.disposable.dispose();
+    }
+
+    private isACryProjFile(e: vscode.TextEditor): boolean {
+        return e.document.fileName.toLowerCase().endsWith(".cryproj");
+        // TODO: 
+        // When CryProj and JSON files will be two different thinghs, change the line above 
+        // with this one
+        //return e.document.languageId.toLowerCase() === "cryproj";
+    }
+}
 
 
 class CryEngineVersion {
@@ -75,10 +94,10 @@ class CryEngineVersion {
 
         let engineVersion = this._getEngineVersion(editor.document);
 
-        
+
         if (engineVersion) {
             // Check if the engineVersion is in the enum of supported CRYENGINE_VERSIONS
-            if (engineVersion in CRYENGINE_VERSIONS){
+            if (engineVersion in CRYENGINE_VERSIONS) {
                 // Update the status bar
                 this._statusBarVersion.text = "CRYENGINE version: " + engineVersion.split("-")[1];
                 this._statusBarVersion.show();
@@ -118,7 +137,7 @@ class CryEngineVersion {
         this._statusBarVersion.dispose();
     }
 
-    public hideStatusBar(){
+    public hideStatusBar() {
         this._statusBarVersion.hide();
     }
 }
